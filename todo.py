56886c9d639bf42ff96f54e93527c6571049aa11
techhub200 <divyanshupant155@gmail.com>
2026-03-12 13:25:13 +0530

    Enhance task management with error handling and Pydantic
    Added error handling for empty tasks and task not found scenarios. Introduced a Task model using Pydantic for better data validation.

--- todo.py
+++ todo.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -7,9 +9,15 @@ tasks = [
     {"task": "build API", "completed": False}
 ]
 
+class Task(BaseModel):
+    task: str
+
+
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+
 #creating a newtask
 @app.post("/new_task")
 def new_task(s: str):
@@ -20,13 +28,33 @@ def new_task(s: str):
     tasks.append(task)
     return {"tasks": tasks}
 
-#get all the task
 @app.get("/my_task")
 def mytask():
+    if not tasks:
+        return {"message": "Empty Task"}
     return tasks
 
 #update task done
-@app.put("/complete_task/{task_id}")
-def complete_task(task_id : int):
-    tasks[task_id]["completed"]=True
-    return {"tasks" : tasks}
+@app.patch("/complete_task/{task_id}")
+def complete_task(task_id: int):
+    
+    if task_id >= len(tasks):
+        return {"error": "Task not found"}
+
+    tasks[task_id]["completed"] = True
+    return {"tasks": tasks}
+
+#delete task
+@app.delete("/delete_task/{task_id}")
+def delete_task(task_id : int):
+    if(task_id>= len(tasks)):
+        return {"error" : "Task not found"}
+    
+    tasks.pop(task_id)
+    return {"tasks": tasks}
+
+
+
+
+
+
